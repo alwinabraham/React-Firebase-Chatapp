24802c18654259b1feb8a5c71e7c81365c990239
alwinabraham <alwinabraham098@gmail.com>
2023-06-07 16:59:49 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState,useRef } from "react"
+import Auth from "./components/Auth"
+import Cookies from 'universal-cookie'
+import Chat from "./components/Chat"
+const cookies = new Cookies()
+
+function App() {
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"))
+  const [room,setRoom] = useState("")
+  
+  const roomInputRef = useRef(null)
+
+  if(!isAuth){
+    return (
+      <div>
+        <Auth setIsAuth={setIsAuth}/>    
+      </div>
+    )
+  }
+  return (
+    <div>{room ?(
+      <Chat />
+    ):(
+      <div className="">
+        <label>Enter Room Name:</label>
+        <input className="border border-gray-900" ref={roomInputRef} />
+        <button className="bg-gray-700 text-white ml-5 rounded-xl p-3 " onClick={()=>setRoom(roomInputRef.current.value)}> Enter Chat</button>
+      </div>
+    )  
+    }
+    </div>
+  )
+}
+
+export default App
---
+++ src/firebase-config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5LzywEMzpYzVXdpW2V3ryQiBB_Y4kYgo",
+  authDomain: "fir-chatapp-42e3b.firebaseapp.com",
+  projectId: "fir-chatapp-42e3b",
+  storageBucket: "fir-chatapp-42e3b.appspot.com",
+  messagingSenderId: "957462110462",
+  appId: "1:957462110462:web:f583be6c9f10ca0f4b45af"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const provider = new GoogleAuthProvider();
